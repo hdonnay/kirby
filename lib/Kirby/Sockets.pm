81f64a60e554c3c3058e4d2cc183f92090363c41
Henry Donnay <hdonnay@gmail.com>
2011-12-05 19:18:43 -0600

    starting on new index page

---
+++ lib/Kirby/Sockets.pm
@@ -0,0 +1,16 @@
+# Part of Kirby.
+# See the COPYING file that should have been distributed with this software.
+# https://raw.github.com/hdonnay/kirby/master/COPYING
+package Kirby::Sockets;
+
+use strict;
+use warnings;
+
+use Mojo::Base 'Mojolicious::Controller';
+
+sub rssSocket {
+    my $self = shift;
+
+}
+
+1;
