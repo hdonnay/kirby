7d9bc762aaf3a36b5413694186c5d2819bb95cf8
Hank Donnay <hank@physics.umn.edu>
2012-04-15 23:54:38 -0500

    shuffle

--- lib/Kirby.pm
+++ lib/Kirby.pm
@@ -4,8 +4,6 @@ use strict;
 use warnings;
 
 use Mojo::Base 'Mojolicious';
-#use Mojo::UserAgent;
-#use FindBin;
 #use Data::Dumper;
 
 #use Kirby::Scraper::SimpleScraper;
@@ -15,87 +13,38 @@ our $VERSION = "0.01";
 sub startup {
     my $self = shift;
 
-
     my $r = $self->routes;
-    #$self->plugin('Kirby::Database');
 
+    # Static boring stuff
+    $r->any('/')->to(controller => 'static', action => 'index');
+    $r->any('/about')->to(controller => 'static', action => 'about');
+
+    # Displaying db items
     my $show = $r->route('/show')->to(controller => 'show');
         $show->route('/issue/:id')->to(action => 'issue');
         $show->route('/series/:title')->to(action => 'series');
+        $show->route('/results')->to(action => 'results');
         $show->route('/all')->to(action => 'all');
 
+    # configuration shit
     my $conf = $r->route('/config')->to(controller => 'config');
         $conf->route('/')->to(action => 'dump');
         $conf->route('/dump')->to(action => 'dump');
         $conf->route('/load')->to(action => 'reload');
         $conf->route('/load')->via('POST')->to(action => 'insert');
 
-    $r->any('/')->to('static#index');
-    $r->any('/about')->to('static#about');
-
-    $r->any('/search' => sub {
-        my $self = shift;
-
-        $self->stash(navbar => {
-            name => "Kirby",
-            index => "/",
-            search => "/search", },
-            action => 'search'
-        );
-
-        my $q = $self->param('q') || undef;
-        if ( defined $q ) {
-            my @results = Kirby::Database->search(q => $q);
-
-            $self->flash(
-                notice => "Results:",
-                results => \@results,
-            );
-            $self->redirect_to('search');
-        }
-        elsif ( defined 'flash' ) {
-            $self->render();
-        }
-        else {
-            $self->flash(alert => "unsuccessful query",);
-            $self->redirect_to('search');
-        };
-    } => 'search');
-
-#    $r->any('/add'  => sub {
-#        my $self = shift;
-#        my $series = $self->param('series') || undef;
-#        my $volume = $self->param('vol') || undef;
-#        my $issue = $self->param('issue') || undef;
-#        my $title = $self->param('title') || 'N/A';
-#        my $description = $self->param('desc') || 'N/A';
-#        if ( (defined $series) and (defined $volume) and (defined $issue) ) {
-#            my $comicID = Kirby::SQL::Kirby->create(
-#                series => $series,
-#                volume => $volume,
-#                issue  => $issue,
-#                title  => $title,
-#                description => $description,
-#            );
-#            $self->render(
-#                text => "Inserted $series $volume $issue",
-#            );
-#        }
-#        else {
-#            $self->render(
-#                text => "Missing required fields",
-#            );
-#        };
-#    });
-
+    # backend things.
+    # 'search' is a ui for backend/dbQuery
+    $r->any('/search')-to(controller => 'backend', action => 'dbQuery');
     my $backend = $r->route('/backend')->to(controller => 'backend');
         $backend->route('/rss')->via('GET')->to(action => 'rssDump');
         $backend->route('/rss')->via('POST')->to(action => 'rssRefresh');
         $backend->route('/add')->via('GET')->to(action => 'lastAddState');
         $backend->route('/add')->via('POST')->to(action => 'add');
+        $backend->route('/dbQuery')->via('GET')->to(action => 'dbQuery');
 
     $self->secret('Kirby Default');
-    $self->defaults(config => $self->plugin('JSONConfig'), );
+    $self->defaults(config => $self->plugin(JSONConfig => {file => 'data/config.json'}) );
     $self->defaults(navbar => undef );
     $self->defaults(tabs => undef );
 }
