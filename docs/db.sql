70f5d865341a330662ac58c3bd82f1abb555e3a9
Hank Donnay <hank@physics.umn.edu>
2012-05-11 21:50:00 -0500

    sql database commands

---
+++ docs/db.sql
@@ -0,0 +1,10 @@
+PRAGMA user_version = 7;
+PRAGMA writable_schema = 1;
+DELETE FROM sqlite_master WHERE TYPE = 'table';
+PRAGMA writable_schema = 0;
+VACUUM;
+PRAGMA INTEGRITY_CHECK;
+CREATE TABLE comics ( id INTEGER PRIMARY KEY, cover BLOB, series TEXT, volume INT, issue INT, title TEXT, description TEXT );
+CREATE TABLE nzb ( id INTEGER PRIMARY KEY, releaseDate TEXT, origYear TEXT, tags TEXT, series TEXT, issue INT, url TEXT, hash TEXT );
+CREATE TABLE history ( id INTEGER PRIMARY KEY, time TEXT, name TEXT, issue INT, action TEXT );
+CREATE TABLE meta ( id INTEGER PRIMARY KEY, time TEXT, asset TEXT, hash TEXT );
